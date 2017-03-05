ac15ee35fb73814363e9e19350ae86a03841d998
Laura McGinn <mcginn.laura@gmail.com>
2017-03-04 21:40:52 -0600

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+//PSEUDOCODE
+// Upon load, the page will be populated with a list of buttons, a blank main area, and a side panel with a submission form.
+
+// When the user presses on a button, a gallery of relevant still gifs load, with info on the rating of each gif.
+
+// The user can click on a gif to animate, and click again to freeze.
+
+// Pressing another button reloads the gallery with gifs relevant to the new topic.
+
+// Putting a term in the search bar will load another button into the list. It will behave like the other buttons.
